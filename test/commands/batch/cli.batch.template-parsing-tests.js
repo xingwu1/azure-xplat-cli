4202a1dde4678a2818d93bd75a90900a003bc2ac
Bevan Arps <bevan.arps@microsoft.com>
2016-11-23 16:08:06 +1300

    Create framework for unit testing of templates

---
+++ test/commands/batch/cli.batch.template-parsing-tests.js
@@ -0,0 +1,50 @@
+//
+// Copyright (c) Microsoft and contributors.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+'use strict';
+
+var should = require('should');
+var fs = require('fs');
+var path = require('path');
+
+var utils = require('../../../lib/util/utils');
+var CLITest = require('../../framework/arm-cli-test');
+var templateUtils = require('../../../lib/commands/batch/batch.templateUtils');
+
+
+describe('cli', function () {
+  describe('batch template parsing', function () {
+    before(function (done) {
+      done();
+    });
+    
+    after(function (done) {
+      done();
+    });
+    
+    beforeEach(function (done) {
+      done();
+    });
+    
+    afterEach(function (done) {
+      done();
+    });
+
+    describe('validateParameterUsage', function () {
+      //TODO
+    });
+
+  });
+});
